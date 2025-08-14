f130b0b7acf8d0647ccf8f5e1319fdb424bd1fae
dinhpt8 <dinhpt8@fpt.com>
2025-08-14 09:34:45 +0700

    update code

--- src/main/java/com/example/demo/service/implement/MicrosoftGraphApiServiceImpl.java
+++ src/main/java/com/example/demo/service/implement/MicrosoftGraphApiServiceImpl.java
@@ -1,7 +1,6 @@
 package com.example.demo.service.implement;
 
 import com.example.demo.dto.response.microsoft.SubscribeResponse;
-import com.microsoft.graph.models.Subscription;
 
 public interface MicrosoftGraphApiServiceImpl {
     SubscribeResponse createNewSubscription();
